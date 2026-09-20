c77e72b9ebddecd92acd010e9902c44e928f116b
abcjuanconde14@gmail.com <Abcjuanconde14@gmail.com>
2026-09-20 10:41:24 -0500

    Fix: corregir sintaxis SQL y tipo de dato en changelog de Liquibase

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -1,5 +1,5 @@
 --changeset estudiante:001
-CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_07001;git status
+CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_07001
 COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
 
 --rollback DROP SCHEMA IF EXISTS workspace.bi_lab_07001;
