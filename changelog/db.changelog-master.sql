00b85b3141efc7bcf6d346fc2da68c49fc218923
abcjuanconde14@gmail.com <Abcjuanconde14@gmail.com>
2026-09-20 01:26:32 -0500

    feat: actualizar changelog y eliminar gitignore

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -1,5 +1,5 @@
---liquibase formatted sql
 --changeset estudiante:001
-CREATE SCHEMA IF NOT EXISTS <workspace>.bi_lab_<codigo_estudiante>
+CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_07001;git status
 COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
---rollback DROP SCHEMA IF EXISTS <workspace>.bi_lab_<codigo_estudiante>;
+
+--rollback DROP SCHEMA IF EXISTS workspace.bi_lab_07001;
